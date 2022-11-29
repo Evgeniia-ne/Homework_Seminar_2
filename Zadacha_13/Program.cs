db1065db5690c1f9cbc4b76652b854749c977551
Asus <koncarik@mail.ru>
2022-11-30 00:39:17 +0500

    Задача решена, программа выводит третью цифру

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number<100)  Console.Write($"{number} -> третьей цифры нет");
+
+else
+{
+    if (number > 999)
+        while (number > 999) number = number/10;
+     Console.Write($"Третья цифра числа: {number%10}");   
+     }
