19defe833386344e56fab3da7cc6a9b66093bbaf
Asus <koncarik@mail.ru>
2022-11-30 00:40:02 +0500

    Задача решена

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 6) Console.WriteLine ($"{number} -> да");
+if (number == 7) Console.WriteLine ($"{number} -> да");
+else Console.WriteLine ($"{number} -> нет");
